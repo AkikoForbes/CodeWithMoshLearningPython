b404a51f4753dcf8c6b2912039e2bcc9217aca4a
AkikoForbes <akiko.forbes@gmail.com>
2020-05-23 22:45:12 +1000

    python strings

--- app.py
+++ app.py
@@ -12,3 +12,45 @@ a = 2 + 3
 b = 4
 score = a + b
 print(score)
+
+students_count = 1000
+rating = 4.99
+is_published = True
+course_name = "Python Programming"
+print(students_count)
+
+print(len(course_name))
+
+print(course_name[0])
+print(course_name[-1])
+print(course_name[0:3])
+print(course_name[0:])
+print(course_name[:3])
+print(course_name[:])
+
+
+# \"
+# \'
+# \\
+# \n
+course = "Python \nPrograming"
+
+print(course)
+
+# formatted strings
+first = "John"
+last = "Forbes"
+
+full = f"{len(first)} {2 + 2}"
+print(full)
+
+course = "   python programing"
+print(course.upper())
+print(course.lower())
+print(course.title())
+print(course.strip())
+print(course.find("Pro"))
+print(course.replace("p", "j"))
+print("pro" in course)
+print("swift" not in course)
+
